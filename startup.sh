4dbe7fd7cca74946511742f0086037942b335761
YM <94254616+Yoxmo@users.noreply.github.com>
2025-03-03 15:41:47 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,10 @@ sudo passwd "$(logname)"
 # Remove keyring files
 rm -rf ~/.local/share/keyrings
 
+sudo sh -c 'echo "IdleActionSec=0" >> /etc/systemd/logind.conf && systemctl restart systemd-logind'
+echo "exclude=kernel*" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+
+
 # Disable and mask dnf-makecache.timer and service
 sudo systemctl disable --now dnf-makecache.timer
 sudo systemctl mask dnf-makecache.service
